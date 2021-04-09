e434d919e0b893c596a021cd297bb03e64687b6c
raw98 <78865860+raw98@users.noreply.github.com>
2021-04-10 00:14:54 +0200

    final2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,20 +8,40 @@ import { NavComponent } from './nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { Router, RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { StudentComponent } from './student/student.component';
+const routes : Routes = [
+  {path : '' , component : HomeComponent},
+  {path : 'home', component : HomeComponent},
+  {path : 'courses', component : CoursesComponent},
+  {path : 'aboutus', component : AboutusComponent},
+  {path : 'contact', component: ContactComponent},
+  {path : '404', component: NotfoundComponent},
+  { path : '**' , redirectTo : '404' }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AboutusComponent,
+    CoursesComponent,
+    ContactComponent,
+    NotfoundComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
